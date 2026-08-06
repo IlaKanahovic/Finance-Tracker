8c3f655fcfd7eb48aee65951704ca73f3d12a3e2
Ila_Kanahovic <computerilakanahovic@gmail.com>
2026-08-06 22:36:59 +0800

    fix: remove basename to Router

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './i18n'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Finance-Tracker">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
